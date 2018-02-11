a946404dd9d34f44fa121b0b716884b9dc84a037
Bruno Barros <brunobarros2014@gmail.com>
2018-02-11 17:09:08 -0300

    separação do JSON de dados da aplicação

---
+++ src/categories.js
@@ -0,0 +1,103 @@
+export const categories =
+[ 
+  {
+    value: 'MY_LIST',
+    label: 'Minha Lista'
+  },
+  {
+    value:'BEST_PICTURE',
+    label: 'Melhor Filme'
+  },
+  {
+    value:'ACTOR_IN_A_LEADING_ROLE',
+    label: 'Melhor Ator'
+  },
+  {
+    value:'ACTRESS_IN_A_LEADING_ROLE',
+    label: 'Melhor Atriz'
+  },
+  {
+    value:'ACTOR_IN_A_SUPPORTING_ROLE',
+    label: 'Melhor Ator Coadjuvante'
+  },
+  {
+    value:'ACTRESS_IN_A_SUPPORTING_ROLE',
+    label: 'Melhor Atriz Coadjuvante'
+  },
+  {
+    value:'ANIMATED_FEATURE_FILM',
+    label: 'Melhor Animação'
+  },
+  {
+    value:'CINEMATOGRAPHY',
+    label: 'Melhor Fotografia'
+  },
+  {
+    value:'COSTUME_DESIGN',
+    label: 'Melhor Figurino'
+  },
+  {
+    value:'DIRECTING',
+    label: 'Melhor Direção'
+  },
+  {
+    value:'DOCUMENTARY_FEATURE',
+    label: 'Melhor Documentário (longa)'
+  },
+  {
+    value:'DOCUMENTARY_SHORT_SUBJECT',
+    label: 'Melhor Documentário (curta)'
+  },
+  {
+    value:'FILM_EDITING',
+    label: 'Melhor Edição'
+  },
+  {
+    value:'FOREIGN_LANGUAGE_FILM',
+    label: 'Melhor Filme Estrangeiro'
+  },
+  {
+    value:'MAKEUP_AND_HAIRSTYLING',
+    label: 'Melhor Maquiagem/Penteado'
+  },
+  {
+    value:'MUSIC_ORIGINAL_SCORE',
+    label: 'Melhor Trilha Sonora'
+  },
+  {
+    value:'MUSIC_ORIGINAL_SONG',
+    label: 'Melhor Canção'
+  },
+  {
+    value:'PRODUCTION_DESIGN',
+    label: 'Melhor Direção de Arte'
+  },
+  {
+    value:'SHORT_FILM_ANIMATED',
+    label: 'Melhor Curta em Animação'
+  },
+  {
+    value:'SHORT_FILM_LIVE_ACTION',
+    label: 'Melhor Curta'
+  },
+  {
+    value:'SOUND_EDITING',
+    label: 'Melhor Edição de Som'
+  },
+  {
+    value:'SOUND_MIXING',
+    label: 'Melhor Mixagem de Som'
+  },
+  {
+    value:'VISUAL_EFFECTS',
+    label: 'Melhores Efeitos Especiais'
+  },
+  {
+    value:'WRITING_ADAPTED_SCREENPLAY',
+    label: 'Melhor Roteiro Adaptado'
+  },
+  {
+    value:'WRITING_ORIGINAL_SCREENPLAY', 
+    label: 'Melhor Roteiro Original'
+  }
+]
